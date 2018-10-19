5549338b710f91ecd1403c4ae03379f82882d2ef
AsovaS-pro <asova@s-pro.io>
2018-10-19 10:40:54 +0300

    hotfix

--- src/requests/aw_docs.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import requests from './requests';
-import API from "../config/api";
-
-export function getAwDocs(pk: number) {
-  const path = API.GET_AW_DOCS;
-  return requests(path, 'get', {}, { planeid: pk });
-}
